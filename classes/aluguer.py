298071934737d5d1657531976c2e441f16ea79ab
T3G3 <152037786+PCII-grupo-T3G3@users.noreply.github.com>
2024-05-03 09:54:20 +0100

    Update aluguer.py

--- classes/aluguer.py
+++ classes/aluguer.py
@@ -1,9 +1,7 @@
-"""
-
-"""""
 # Class OrderProduct
 from classes.cliente import Cliente
 from classes.veiculo import Veiculo
+import datetime
 # Import the generic class
 from classes.gclass import Gclass
 
@@ -14,13 +12,13 @@ class Aluguer(Gclass):
     sortkey = ''
     nkey = 2
     # class attributes, identifier attribute must be the first one on the list
-    att = ['_aluguer','_placa','_dias','_preco']
+    att = ['_aluguer','_placa','_dias','_preco','_datainicio','_datafinal']
     # Class header title
     header = 'Aluguer'
     # field description for use in, for example, in input form
-    des = ['Código de aluguer','Placa','Número de dias','Preço']
+    des = ['Código de aluguer','Placa','Número de dias','Preço', 'Data de início','Data final']
     # Constructor: Called when an object is instantiated
-    def __init__(self, aluguer, cliente, placa, dias, preco):
+    def __init__(self, aluguer, cliente, placa, dias, preco, datainicio, datafinal):
         super().__init__()
         # Object attributes
         # Check the order and product referential integrity
@@ -30,6 +28,8 @@ class Aluguer(Gclass):
                 self._placa = placa
                 self._dias = int(dias)
                 self._preco = float(preco)
+                self._datainicio = datetime.date.fromisoformat(datainicio)
+                self._datafinal = datetime.date.fromisoformat(datafinal)
                 # Add the new object to the OrderProduct list
                 Aluguer.obj[placa] = self
                 Aluguer.lst.append(placa)
@@ -62,4 +62,20 @@ class Aluguer(Gclass):
     @preco.setter
     def preco(self, preco):
         self._preco = float(preco)
+    # first date getter method
+    @property
+    def datainicio(self):
+        return self._datainicio
+    # last date getter method
+    @property
+    def datafinal(self):
+        return self._datafinal
+    # first date property setter method
+    @datainicio.setter
+    def datainicio(self, datai):
+        self._datainicio = datetime.date.fromisoformat(datai)
+    # last date property setter method
+    @datafinal.setter
+    def datainicio(self, dataf):
+        self._datafinal = datetime.date.fromisoformat(dataf)
         
