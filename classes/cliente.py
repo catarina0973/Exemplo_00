0b2474dc2a8da00ece4f62f3622f379ad29c086e
T3G3 <152037786+PCII-grupo-T3G3@users.noreply.github.com>
2024-05-03 09:55:18 +0100

    Update cliente.py

--- classes/cliente.py
+++ classes/cliente.py
@@ -4,25 +4,27 @@ Created on Tue Apr  2 08:36:29 2024
 
 @author: cacfa
 """
-
 from classes.gclass import Gclass
-import datetime
+
 class Cliente(Gclass):
     obj = dict()
     lst = list()
     pos = 0
     sortkey = ''
     auto_number = 0
-    att=['_nome','_endereco','_telemovel','_senha','_code']
-    def __init__(self, nome, endereco, telemovel, senha, code=None):
+    nkey = 1
+    att=['_code','_nome','_endereco','_telemovel','_senha']
+    def __init__(self, code, nome, endereco, telemovel, senha):
         super().__init__()
         self._code=code
         self._nome=str(nome)
         self._endereco=str(endereco)
         self._telemovel=telemovel
         self._senha=senha
+        
         Cliente.obj[code]=self
         Cliente.lst.append(code)
+    
     @property 
     def code(self):
         return self._code
@@ -30,7 +32,9 @@ class Cliente(Gclass):
     @property 
     def nome(self):
         return self._nome
-    
+    @nome.setter 
+    def nome(self, nome):
+        self._nome=nome
     @property 
     def endereco(self):
         return self._endereco
@@ -52,4 +56,3 @@ class Cliente(Gclass):
     def clear():
         Cliente.obj.clear()
         Cliente.lst.clear()
-        
