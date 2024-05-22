fa4bad4f26b9257e5b13390a60212b3374237872
Catarina Almeida <152037786+catarinaalmeida7372@users.noreply.github.com>
2024-05-22 08:46:20 +0100

    Update admnistradores.py

--- classes/admnistradores.py
+++ classes/admnistradores.py
@@ -1,42 +1,49 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 30 08:36:44 2024
-
-@author: cacfa
-"""
 
 from classes.gclass import Gclass
-import datetime
+
 class Admnistradores(Gclass):
     obj = dict()
     lst = list()
     pos = 0
     sortkey = ''
+    nkey = 1
     auto_number = 0
-    att=['_nome','__senha']
-    def __init__(self, nome,senha):
+    # class attributes, identifier attribute must be the first one on the list
+    att = ['_code','_senha']
+    # Class header title
+    header = 'Admnistrador'
+    
+    # field description for use in, for example, in input form
+    des = ['User','Password']
+    # Constructor: Called when an object is instantiated
+    def __init__(self, code,senha):
         super().__init__()
+        # Object attributes
+        self._code = code
         
-        self._nome=str(nome)
+        self._senha = senha
         
-        self._senha=senha
-        Admnistradores.obj[nome]=self
-        Admnistradores.lst.append(nome)
+        Admnistradores.obj[code] = self
+        Admnistradores.lst.append(code)
+
+    # Object properties
+    # getter methodes
+  
+    @property
+    def code(self):
+        return self._code
     
-    
-    @property 
-    def nome(self):
-        return self._nome
-    
+    @code.setter
+    def code(self, name):
+        self._code = name
     @property 
     def senha(self):
         return self._senha
-    @senha.setter 
+    @senha.setter
     def senha(self, senha):
-        self._senha=senha
-    
+        self._senha = senha
+        
     def clear():
         Admnistradores.obj.clear()
         Admnistradores.lst.clear()
-        
-
