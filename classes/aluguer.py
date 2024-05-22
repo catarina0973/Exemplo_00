ec7b39f6e4a0a0abb1f114cdfbcc63bbd4261aff
Catarina Almeida <152037786+catarinaalmeida7372@users.noreply.github.com>
2024-05-22 08:47:11 +0100

    Update aluguer.py

--- classes/aluguer.py
+++ classes/aluguer.py
@@ -1,51 +1,66 @@
-# Class OrderProduct
 from classes.cliente import Cliente
-from classes.veiculo import Veiculo
+
 import datetime
 # Import the generic class
 from classes.gclass import Gclass
-
+from classes.veiculo import Veiculo
+from classes.cliente import Cliente
 class Aluguer(Gclass):
     obj = dict()
     lst = list()
     pos = 0
     sortkey = ''
-    nkey = 2
+    nkey = 1
+    auto_number = 0
     # class attributes, identifier attribute must be the first one on the list
-    att = ['_aluguer','_placa','_dias','_preco','_datainicio','_datafinal']
+    att = ['_code','_user','_dias','_datainicio','_datafinal','_precodiario','_precototal','_recolha']
     # Class header title
     header = 'Aluguer'
     # field description for use in, for example, in input form
-    des = ['Código de aluguer','Placa','Número de dias','Preço', 'Data de início','Data final']
+    des = ['Modelo','User','Número de dias', 'Data de início','Data final','Preço Diário','Preço Total (preço diário*nº de dias)','Local de Recolha']
     # Constructor: Called when an object is instantiated
-    def __init__(self, aluguer, cliente, placa, dias, preco_veiculo, datainicio, datafinal):
+    def __init__(self, code, user, dias,  datainicio,datafinal,precodiario,precototal,recolha):
         super().__init__()
         # Object attributes
         # Check the order and product referential integrity
-        if cliente in Cliente.lst:
-            if placa in Veiculo.lst:
-                self._aluguer = aluguer
-                self._placa = placa
+        if user in Cliente.lst:
+            if code in Veiculo.lst:
+                self._code=code #o code é o modelo do veiculo
+                self._user = user
                 self._dias = int(dias)
-                self._preco_veiculo = float(preco_veiculo)
+                self._precodiario = precodiario
+                self._precototal = precototal
                 self._datainicio = datetime.date.fromisoformat(datainicio)
                 self._datafinal = datetime.date.fromisoformat(datafinal)
-                # Add the new object to the OrderProduct list
-                Aluguer.obj[placa] = self
-                Aluguer.lst.append(placa)
+                self._recolha=recolha
+            # Add the new object to the OrderProduct list
+                Aluguer.obj[code] = self
+                Aluguer.lst.append(code)
             else:
-                print('Aluguer ', placa, ' inexistente.')
+                print('Aluguer ', code, ' inexistente.')
         else:
-            print('Aluguer', placa, ' inexistente.')
+            print('Aluguer', code, ' inexistente.')
+    
     # Object properties
-    # order property getter method
+
     @property
-    def aluguer(self):
-        return self._aluguer
-    # product property getter method
+    def recolha(self):
+        return self._recolha
+    @recolha.setter
+    def recolha(self,local):
+        self._recolha=local
     @property
-    def placa(self):
-        return self._placa
+    def code(self):
+        return self._code
+    @code.setter
+    def code(self,user):
+        self._coder=user
+    @property
+    def user(self):
+        return self._user
+    @user.setter
+    def user(self, user):
+        self._user = user
     # quantity property getter method
     @property
     def dias(self):
@@ -56,12 +71,21 @@ class Aluguer(Gclass):
         self._dias = int(dias)
     # price property getter method
     @property
-    def preco_veiculo(self):
-        return self._preco_veiculo
+    def precodiario(self):
+        
+        return self._precodiario
     # price property setter method
-    @preco_veiculo.setter
-    def preco_veiculo(self, preco):
-        self._preco_veiculo = float(preco)
+    @precodiario.setter
+    def precodiario(self, preco):
+        self._precodiario = float(preco)
+    @property
+    def precototal(self):
+        
+        return self._precototal
+    # price property setter method
+    @precodiario.setter
+    def precototal(self, preco):
+        self._precototal = float(preco)
     # first date getter method
     @property
     def datainicio(self):
@@ -78,6 +102,3 @@ class Aluguer(Gclass):
     @datafinal.setter
     def datainicio(self, dataf):
         self._datafinal = datetime.date.fromisoformat(dataf)
-    def preco_aluguer(self):
-        p=self._preco_veiculo*self._dias
-        return p
