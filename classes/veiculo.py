384ae40a14b27dc22c87669d20426456e409c664
Catarina Almeida <152037786+catarinaalmeida7372@users.noreply.github.com>
2024-05-22 08:48:34 +0100

    Update veiculo.py

--- classes/veiculo.py
+++ classes/veiculo.py
@@ -1,5 +1,3 @@
-# Class Product
-# Import the generic class
 from classes.gclass import Gclass
 
 class Veiculo(Gclass):
@@ -7,52 +5,67 @@ class Veiculo(Gclass):
     lst = list()
     pos = 0
     sortkey = ''
+    auto_number = 0 # = 1 in case of auto number on
     nkey = 1
     # class attributes, identifier attribute must be the first one on the list
-    att = ['_placa','_modelo','_ano','_valorDiario','_disponivel']
+    att = ['_code','_modelo','_ano','_valordiario', '_disponivel','_recolha']
     # Class header title
-    header = 'Veículos'
+    header = 'Veículo'
     # field description for use in, for example, in input form
-    des = ['Placa','Modelo','Ano','Valor Diário','Disponível']
+    des = ['Code','Modelo','Ano','Valor Diário', 'Disponível','Local de Recolha']
     # Constructor: Called when an object is instantiated
-    def __init__(self, placa, modelo, ano, valorDiario, disponivel):
+    def __init__(self, code, modelo, ano, valordiario, disponivel, recolha):
         super().__init__()
-        self._placa = placa
-        self._modelo = modelo
-        self._ano = int(ano)
-        self._valorDiario = float(valorDiario)
-        self._disponivel=bool(disponivel)
-        # Add the new object to the Product list
-        Veiculo.obj[placa] = self
-        Veiculo.lst.append(placa)
-    # Object properties
-    # placa property getter method
-    @property
-    def placa(self):
-        return self._placa
-    # modelo property getter method
-    @property
+        
+        self._code=code
+        self._modelo=modelo
+        self._ano=ano
+        self._valordiario=valordiario
+        self._disponivel=disponivel
+        self._recolha=recolha
+        Veiculo.obj[code]=self
+        Veiculo.lst.append(code)
+    
+    
+    
+    @property 
+    def code(self):
+        return self._code
+    @code.setter 
+    def code(self, nome):
+        self._code=nome
+    @property 
+    def recolha(self):
+        return self._recolha
+    @recolha.setter 
+    def recolha(self,local):
+        self._recolha=local
+    @property 
     def modelo(self):
         return self._modelo
-    # ano property getter method
-    @property
+    @modelo.setter 
+    def modelo(self, model):
+        self._modelo=model
+        
+    @property 
     def ano(self):
         return self._ano
-    # valorDiario property getter method
-    @property
-    def valorDiario(self):
-        return self._valorDiario
-    # disponivel property getter method
-    @property
+    @ano.setter 
+    def ano(self, ano):
+        self._ano=ano
+    @property 
+    def valordiario(self):
+        return self._valordiario
+    @valordiario.setter 
+    def valordiario(self, valordiario):
+        self._valordiario=valordiario
+    @property 
     def disponivel(self):
         return self._disponivel
-    # valorDiario property setter method
-    @valorDiario.setter
-    def valorDiario(self, valorDiario):
-        self._valorDiario = valorDiario
-    # disponivel property setter method
-    @disponivel.setter
-    def disponivel(self, disponivel):
-        self._disponivel=disponivel
-
-
+    @disponivel.setter 
+    def disponivel(self, valor):
+        self._disponivel= valor
+    def clear():
+        Veiculo.obj.clear()
+        Veiculo.lst.clear()
+        
