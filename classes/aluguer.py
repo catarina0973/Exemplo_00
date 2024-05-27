ab0f682a8dad1977fda4068251dd36eeb9bb3757
Lara Parente <161622396+up202304673@users.noreply.github.com>
2024-05-28 00:57:05 +0100

    Update aluguer.py

--- classes/aluguer.py
+++ classes/aluguer.py
@@ -9,7 +9,7 @@ from classes.userlogin import Userlogin
 
 class Aluguer(Gclass):
     obj = dict()
-    
+    mlist= list()
     iobj =dict()
     fobj=dict()
     lst = list()
@@ -18,19 +18,26 @@ class Aluguer(Gclass):
     pos = 0
     sortkey = ''
     nkey = 1
-    auto_number = 0
+    auto_number = 1
     # class attributes, identifier attribute must be the first one on the list
-    att = ['_code','_user','_dias','_datainicio','_datafinal','_precodiario','_precototal','_recolha']
+    att = ['_code','_modelo','_user','_dias','_datainicio','_datafinal','_precodiario','_precototal','_recolha']
     # Class header title
     header = 'Aluguer'
     # field description for use in, for example, in input form
-    des = ['Modelo','User','Número de dias', 'Data de início (AAAA-MM-DD)','Data final (AAAA-MM-DD)','Preço Diário','Preço Total (preço diário*nº de dias)','Local de Recolha']
+    des = ['Code','Modelo','User','Número de dias', 'Data de início (AAAA-MM-DD)','Data final (AAAA-MM-DD)','Preço Diário','Preço Total (preço diário*nº de dias)','Local de Recolha']
     # Constructor: Called when an object is instantiated
-    def __init__(self, code, user, dias,  datainicio, datafinal, precodiario, precototal,recolha):
+    def __init__(self, code,modelo, user, dias,  datainicio, datafinal, precodiario, precototal,recolha):
         super().__init__()
+        if code == 'None':
+            codes = Aluguer.getatlist('_code')
+            if codes == []:
+                code = str(1)
+            else:
+                code = str(max(map(int, Aluguer.getatlist('_code'))) + 1)
+        # Object attributes
         # Object attributes
         # Check the order and product referential integrity
-        
+        self._modelo = modelo
         self._code=code #o code é o modelo do veiculo
         self._user = user
         self._dias = int(dias)
@@ -43,9 +50,13 @@ class Aluguer(Gclass):
         Aluguer.flist.append(datetime.date.fromisoformat(datafinal))
         Aluguer.obj[code] = self
         Aluguer.lst.append(code)
-        
-        
-        
+        Aluguer.mlist.append(modelo)
+    @property 
+    def modelo(self):
+        return self._modelo
+    @modelo.setter 
+    def modelo(self, modelo):
+        self._modelo = modelo
     @property
     def recolha(self):
         return self._recolha
@@ -110,7 +121,7 @@ class Aluguer(Gclass):
     def chk_validity(self):
         message = 'Aprovado!'
         
-        if self._code not in Veiculo.obj.keys():            
+        if self._modelo not in Veiculo.obj.keys():            
             return 'Veículo não encontrado!'
             Aluguer.ilist.remove(self._datainicio)
             Aluguer.flist.remove(self._datafinal)
@@ -120,18 +131,19 @@ class Aluguer(Gclass):
             return 'User não encontrado!'
         elif (self._datafinal - self._datainicio).days != int(self._dias):
             return 'As datas não correspondem ao número de dias!'
-        
-        elif Aluguer.lst.count(self._code)>1 and (Aluguer.flist.count(self._datafinal)>1 or Aluguer.ilist.count(self._datainicio)>1 ):
-            message=f'O veículo já está alugado de {self._datainicio} a {self._datafinal}'
+        if self._modelo in Veiculo.obj.keys(): 
+            
+            if Aluguer.mlist.count(self._modelo)>1 and (Aluguer.flist.count(self._datafinal)>1 or Aluguer.ilist.count(self._datainicio)>1 ):
+                message=f'O veículo já está alugado de {self._datainicio} a {self._datafinal}'
+                Aluguer.ilist.remove(self._datainicio)
+                Aluguer.flist.remove(self._datafinal)
+                Aluguer.mlist.remove(self._modelo)
+        elif self._recolha != Veiculo.robj.get(self._modelo):
             Aluguer.ilist.remove(self._datainicio)
             Aluguer.flist.remove(self._datafinal)
-            Aluguer.lst.remove(self._code)
-        elif self._recolha != Veiculo.robj.get(self._code):
-            Aluguer.ilist.remove(self._datainicio)
-            Aluguer.flist.remove(self._datafinal)
-            return f'Local de recolha: {Veiculo.robj.get(self._code)}'
+            return f'Local de recolha: {Veiculo.robj.get(self._modelo)}'
         
-        self._precototal = float(Veiculo.vobj[self._code]) * int(self._dias)
-        self._precodiario = Veiculo.vobj[self._code]
+        self._precototal = float(Veiculo.vobj[self._modelo]) * int(self._dias)
+        self._precodiario = Veiculo.vobj[self._modelo]
         
         return message
